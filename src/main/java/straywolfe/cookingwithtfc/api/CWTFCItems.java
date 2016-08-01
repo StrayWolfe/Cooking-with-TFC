6e8e1b04eea3a828f351eaf269a4bda5cd36e763
JAWolfe04 <JAWolfe04@msn.com>
2016-08-01 18:02:59 -0500

    Show TFC Foods in creative menu

--- src/main/java/straywolfe/cookingwithtfc/api/CWTFCItems.java
+++ src/main/java/straywolfe/cookingwithtfc/api/CWTFCItems.java
@@ -3,12 +3,10 @@ package straywolfe.cookingwithtfc.api;
 import java.util.ArrayList;
 
 import com.bioxx.tfc.Items.ItemTerra;
-import com.bioxx.tfc.api.TFCItems;
 
 import net.minecraft.item.Item;
 import straywolfe.cookingwithtfc.common.item.CWTFCItemRegistry;
 import straywolfe.cookingwithtfc.common.item.CWTFCItemSetup;
-import straywolfe.cookingwithtfc.common.lib.Settings;
 
 public class CWTFCItems 
 {	
@@ -147,106 +145,7 @@ public class CWTFCItems
 	public static Item ClayCookingPot;
 	
 	public static void initialise()
-	{
-		/*///////////////////////////////////////////////
-		 * Remove Items being replaced from Creative Tabs
-		 *///////////////////////////////////////////////
-		
-		if(Settings.hideTFCFoods)
-		{		
-			//Fruit
-			TFCItems.redApple.setCreativeTab(null);
-			TFCItems.banana.setCreativeTab(null);
-			TFCItems.orange.setCreativeTab(null);
-			TFCItems.greenApple.setCreativeTab(null);
-			TFCItems.lemon.setCreativeTab(null);
-			TFCItems.olive.setCreativeTab(null);
-			TFCItems.cherry.setCreativeTab(null);
-			TFCItems.peach.setCreativeTab(null);
-			TFCItems.plum.setCreativeTab(null);
-			
-			TFCItems.wintergreenBerry.setCreativeTab(null);
-			TFCItems.blueberry.setCreativeTab(null);
-			TFCItems.raspberry.setCreativeTab(null);
-			TFCItems.strawberry.setCreativeTab(null);
-			TFCItems.blackberry.setCreativeTab(null);
-			TFCItems.bunchberry.setCreativeTab(null);
-			TFCItems.cranberry.setCreativeTab(null);
-			TFCItems.snowberry.setCreativeTab(null);
-			TFCItems.elderberry.setCreativeTab(null);
-			TFCItems.gooseberry.setCreativeTab(null);
-			TFCItems.cloudberry.setCreativeTab(null);
-			
-			//Vegetables
-			TFCItems.tomato.setCreativeTab(null);
-			TFCItems.potato.setCreativeTab(null);
-			TFCItems.onion.setCreativeTab(null);
-			TFCItems.cabbage.setCreativeTab(null);
-			TFCItems.garlic.setCreativeTab(null);
-			TFCItems.carrot.setCreativeTab(null);
-			TFCItems.greenbeans.setCreativeTab(null);
-			TFCItems.greenBellPepper.setCreativeTab(null);
-			TFCItems.yellowBellPepper.setCreativeTab(null);
-			TFCItems.redBellPepper.setCreativeTab(null);
-			TFCItems.squash.setCreativeTab(null);
-			TFCItems.seaWeed.setCreativeTab(null);
-			
-			//Proteins
-			TFCItems.porkchopRaw.setCreativeTab(null);
-			TFCItems.fishRaw.setCreativeTab(null);
-			TFCItems.beefRaw.setCreativeTab(null);
-			TFCItems.chickenRaw.setCreativeTab(null);
-			TFCItems.soybean.setCreativeTab(null);
-			TFCItems.egg.setCreativeTab(null);
-			TFCItems.eggCooked.setCreativeTab(null);
-			TFCItems.calamariRaw.setCreativeTab(null);
-			TFCItems.muttonRaw.setCreativeTab(null);
-			TFCItems.venisonRaw.setCreativeTab(null);
-			TFCItems.horseMeatRaw.setCreativeTab(null);
-			
-			//Dairy
-			TFCItems.cheese.setCreativeTab(null);
-			TFCItems.woodenBucketMilk.setCreativeTab(null);
-			
-			//Grains
-			TFCItems.barleyWhole.setCreativeTab(null);
-			TFCItems.oatWhole.setCreativeTab(null);
-			TFCItems.riceWhole.setCreativeTab(null);
-			TFCItems.ryeWhole.setCreativeTab(null);
-			TFCItems.wheatWhole.setCreativeTab(null);
-			TFCItems.maizeEar.setCreativeTab(null);
-			
-			TFCItems.barleyGrain.setCreativeTab(null);
-			TFCItems.oatGrain.setCreativeTab(null);
-			TFCItems.riceGrain.setCreativeTab(null);
-			TFCItems.ryeGrain.setCreativeTab(null);
-			TFCItems.wheatGrain.setCreativeTab(null);
-			
-			TFCItems.barleyGround.setCreativeTab(null);
-			TFCItems.cornmealGround.setCreativeTab(null);
-			TFCItems.oatGround.setCreativeTab(null);
-			TFCItems.riceGround.setCreativeTab(null);
-			TFCItems.ryeGround.setCreativeTab(null);
-			TFCItems.wheatGround.setCreativeTab(null);
-			
-			TFCItems.barleyDough.setCreativeTab(null);
-			TFCItems.cornmealDough.setCreativeTab(null);
-			TFCItems.oatDough.setCreativeTab(null);
-			TFCItems.riceDough.setCreativeTab(null);
-			TFCItems.ryeDough.setCreativeTab(null);
-			TFCItems.wheatDough.setCreativeTab(null);
-			
-			TFCItems.barleyBread.setCreativeTab(null);
-			TFCItems.cornBread.setCreativeTab(null);
-			TFCItems.oatBread.setCreativeTab(null);
-			TFCItems.riceBread.setCreativeTab(null);
-			TFCItems.ryeBread.setCreativeTab(null);
-			TFCItems.wheatBread.setCreativeTab(null);
-			
-			TFCItems.sugarcane.setCreativeTab(null);
-			TFCItems.sugar.setCreativeTab(null);
-		}
-		
+	{		
 		//Setup Items
 		CWTFCItemSetup.ItemSetup();
 		
